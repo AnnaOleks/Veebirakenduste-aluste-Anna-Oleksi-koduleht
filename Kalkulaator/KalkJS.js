b8e8b7d760093a5dc01203cd39e97e75c63e5314
AnnaO <annaoleks88@gmail.com>
2025-03-02 15:08:32 +0200

    Kalkulaator

--- Kalkulaator/KalkJS.js
+++ Kalkulaator/KalkJS.js
@@ -1,5 +1,68 @@
+function valuutasoov(){
+    let soov = document.getElementById("soov");
+    let usa=document.getElementById("usa");
+    let rus=document.getElementById("rus");
+    let lit=document.getElementById("lit");
+    let lat=document.getElementById("lat");
+    let ingl=document.getElementById("ingl");
+    let valuutapilt=document.getElementById("valuutapilt");
+
+    if (usa.checked){
+        soov.innerHTML=usa.value;
+        valuutapilt.src="imgVal/usa.jpg";
+    } else if(rus.checked){
+        soov.innerHTML=rus.value;
+        valuutapilt.src="imgVal/rus.jpg";
+    } else if(lit.checked){
+        soov.innerHTML=lit.value;
+        valuutapilt.src="imgVal/lit.png";
+    } else if(lat.checked){
+        soov.innerHTML=lat.value;
+        valuutapilt.src="imgVal/lat.png";
+    } else if(ingl.checked){
+        soov.innerHTML=ingl.value;
+        valuutapilt.src="imgVal/engl.jpg";
+    } else {
+        soov.innerHTML="Pead midagi valima";
+    }
+}
+
+function arvutamine(euro,hind){
+    return (euro*hind).toFixed(2);
+}
+
+const usahind=1.0626;
+const rushind=98.3126;
+const lathind =0.9136;
+const lithind=4.4886;
+const inglhind=0.8329;
+
+function kokku(){
+    let summakokku = document.getElementById("summakokku");
+    let eurot=document.getElementById("eurot");
+    let usa=document.getElementById("usa");
+    let lat=document.getElementById("lat");
+    let rus=document.getElementById("rus");
+    let lit=document.getElementById("lit");
+    let ingl=document.getElementById("ingl");
+
+    if(usa.checked){
+        summakokku.innerHTML= "Sina saad: " + arvutamine(eurot.value, usahind) + " " + usa.value;
+    } else if(rus.checked){
+        summakokku.innerHTML= "Sina saad: " + arvutamine(eurot.value, rushind) + " " + rus.value;
+    } else if(lat.checked){
+        summakokku.innerHTML= "Sina saad: " + arvutamine(eurot.value, lathind) + " " + lat.value;
+    } else if(lit.checked){
+        summakokku.innerHTML= "Sina saad: " + arvutamine(eurot.value, lithind) + " " + lit.value;
+    } else if(ingl.checked) {
+        summakokku.innerHTML = "Sina saad: " + arvutamine(eurot.value, inglhind) + " " + ingl.value;
+    } else
+        summakokku.innerHTML= "Pead midagi valima";
+}
+
 function autovalikJS(){
-    let valik = document.getElementById("valik");
+    let valikID = document.getElementById("valikID");
+    let valikpilt=document.getElementById("valikpilt");
     let auto1 = document.getElementById("auto1");
     let auto2 = document.getElementById("auto2");
     let auto3 = document.getElementById("auto3");
@@ -8,23 +71,23 @@ function autovalikJS(){
     let auto6 = document.getElementById("auto6");
 
     if (auto1.checked){
-        valik.innerHTML=
-            "<img src='img/AudiA5.jpg' width='200'><br>Audi";
+        valikID.innerHTML=auto1.value;
+        valikpilt.src="img/AudiA5.jpg";
     } else if (auto2.checked){
-        valik.innerHTML=
-            "<img src='img/BMWm6.jpg' width='200'><br>BMW";
+        valikID.innerHTML=auto2.value;
+        valikpilt.src="img/BMWm6.jpg";
     } else if (auto3.checked){
-        valik.innerHTML=
-            "<br> <img src='img/JAGUARXF.jpg' width='200'><br>Jaguar";
+        valikID.innerHTML=auto3.value;
+        valikpilt.src="img/JAGUARXF.jpg";
     } else if (auto4.checked){
-        valik.innerHTML=
-            "<br> <img src='img/MercedesCLS63AMG.jpg' width='200'><br>Mercedes-Benz";
+        valikID.innerHTML=auto4.value;
+        valikpilt.src="img/MercedesCLS63AMG.jpg";
     } else if (auto5.checked){
-        valik.innerHTML=
-            "<br> <img src='img/ChevroletCamaro.jpg' width='200'><br>Chevrolet";
+        valikID.innerHTML=auto5.value;
+        valikpilt.src="img/ChevroletCamaro.jpg";
     } else if (auto6.checked){
-        valik.innerHTML=
-            "<br> <img src='img/FordMustangV6.jpg' width='200'><br>Ford";
+        valikID.innerHTML=auto6.value;
+        valikpilt.src="img/FordMustangV6.jpg";
     } else {
         valik.innerHTML="Sa pead midagi valima"
     }
